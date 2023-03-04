27445ef0ea6109b75867c9667f0f1dadcd3886eb
BabacarSylla <babisko9817@gmail.com>
2023-03-04 10:57:46 +0000

    firts commit

---
+++ src/main/java/com/groupeisi/examenspringdevops/repository/ICVRepository.java
@@ -0,0 +1,12 @@
+package com.groupeisi.examenspringdevops.repository;
+
+import com.groupeisi.examenspringdevops.entity.CVEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ICVRepository extends JpaRepository<CVEntity, Integer> {
+    CVEntity findByEmail(String email);
+
+}
